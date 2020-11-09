e383b6a36c2eb70e6007e3070bdc8f4a53faf5e7
Lei <gabriel_sl19798@hotmail.com>
2020-11-10 09:02:40 +1300

    index page and menu page

---
+++ include/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Include Exercise</title>
+</head>
+<body>
+<div id="header">Menu, login, pictures</div>
+<div id="main">Contents</div>
+<div id="footer">contact information</div>
+</body>
+</html>
---
+++ include/page1.php
@@ -0,0 +1,5 @@
+<ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="aboutUs.php">About Us</a></li>
+    <li><a href="Products.php">Products</a></li>
+</ul>
