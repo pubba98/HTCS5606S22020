b9edb565115653b6400fe1b5e283b46d56ef23a2
Lei <gabriel_sl19798@hotmail.com>
2020-11-11 14:23:02 +1300

    have an email sending

--- sentemail/sendemail.php
+++ sentemail/sendemail.php
@@ -1,20 +1,14 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
+# Include the Autoloader (see "Libraries" for install instructions)
 require 'vendor/autoload.php';
 use Mailgun\Mailgun;
-
 # Instantiate the client.
-$mgClient = Mailgun::create('eacf1f278c4e1854aa914fe8d7f16ff5-ba042922-041df505', 'https://API_HOSTNAME');
-$domain = "www.unitec.ac.nz";
-$params = array(
-    'from'    => 'Excited User <lsong@unitec.ac.nz>',
-    'to'      => 'gabriel_sl19798@hotmail.com',
-    'subject' => 'Hello',
-    'text'    => 'Testing some Mailgun awesomness!'
-);
-
+$mgClient = new Mailgun('eacf1f278c4e1854aa914fe8d7f16ff5-ba042922-041df505');
+$domain = "lsong@unitec.ac.nz";
 # Make the call to the client.
-$mgClient->messages()->send($domain, $params);
+$result = $mgClient->sendMessage($domain, array(
+    'from'	=> 'Excited User <lsong@unitec.ac.nz>',
+    'to'	=> 'Baz <gabriel_sl19798@hotmail.com>',
+    'subject' => 'Hello',
+    'text'	=> 'Testing some Mailgun awesomness!'
+));
