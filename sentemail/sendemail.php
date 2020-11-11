1809f3fff01db3acc554b03dd838de1cbb3afcc0
Lei <gabriel_sl19798@hotmail.com>
2020-11-11 14:25:52 +1300

    have an email sending

--- sentemail/sendemail.php
+++ sentemail/sendemail.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 # Include the Autoloader (see "Libraries" for install instructions)
 require 'vendor/autoload.php';
 use Mailgun\Mailgun;
