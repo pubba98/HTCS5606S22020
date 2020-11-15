b4548722490dc6c11d857c0886659f460b6971d6
Lei <gabriel_sl19798@hotmail.com>
2020-11-16 09:58:44 +1300

    created a product page

---
+++ include/products.php
@@ -0,0 +1,25 @@
+<?php
+//1. connect to database
+$server = "klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$dbusername = "t8jnow42fmp1smpt";
+$dbpassword = "fdavedw769oxw5pd";
+$dbname = "k2nfay1osz1i59kc";
+
+$conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
+
+//2. create a query
+$sql = "select * from products";
+
+//3. run the query on that connection
+$result = mysqli_query($conn,$sql);
+
+//4. show result
+while ($row = $result->fetch_assoc()){
+    ?>
+    <div>
+        <p><?php echo $row["name"]; ?></p>
+        <p><?php echo $row["price"]; ?></p>
+        <p><img src="<?php echo $row["image"]; ?>"</p>
+    </div>
+    <?php
+}
