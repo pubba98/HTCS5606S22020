4acae41b7f6c13b3c0294971e95529b371110a0e
Lei <gabriel_sl19798@hotmail.com>
2020-11-17 09:21:15 +1300

    Made registration page

---
+++ include/register.php
@@ -0,0 +1,29 @@
+<?php
+$firstname = $_POST["firstname"];
+$lastname = $_POST["lastname"];
+$username = $_POST["username"];
+$password = $_POST["password"];
+$address = $_POST["address"];
+$phone = $_POST["phone"];
+
+//1. connect to database
+$server = "klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$dbusername = "t8jnow42fmp1smpt";
+$dbpassword = "fdavedw769oxw5pd";
+$dbname = "k2nfay1osz1i59kc";
+
+$conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
+
+//2. create a query
+$sql = "INSERT INTO `users`(`id`, `firstname`, `lastname`, `username`, `password`, `address`, `phoneNumber`) 
+VALUES (NULL,'$firstname','$lastname','$username','$password','$address','$phone')";
+
+//3. run the query
+if (mysqli_query($conn, $sql)){
+    echo "data has been inserted";
+}else{
+    echo "data has not been inserted";
+}
+
+
+echo "<a href='index.php'>Home</a>";
