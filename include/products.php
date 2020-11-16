349f16174c8cebe8e4c24c8d33a50ed4ffe103ba
Lei <gabriel_sl19798@hotmail.com>
2020-11-17 10:36:08 +1300

    created a shopping cart

--- include/products.php
+++ include/products.php
@@ -32,7 +32,7 @@ while ($row = $result->fetch_assoc()){
         <p><img src="<?php echo $row["image"]; ?>"</p>
         <form action="..." method="post">
             <input name="productID" value="<?php echo $row["id"]; ?>" type="hidden">
-            <input name="qty" type="number" placeholder="QTY">
+            <input name="qty" type="number" placeholder="QTY" min="0">
             <input type="submit" value="Add to cart">
         </form>
     </div>
