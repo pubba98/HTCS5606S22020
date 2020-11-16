52f7845e38c8359c73afd9fe4117f96b1c642446
Lei <gabriel_sl19798@hotmail.com>
2020-11-17 10:06:18 +1300

    create a add to cart form for each product

--- include/products.php
+++ include/products.php
@@ -30,6 +30,11 @@ while ($row = $result->fetch_assoc()){
         <p><?php echo $row["name"]; ?></p>
         <p><?php echo $row["price"]; ?></p>
         <p><img src="<?php echo $row["image"]; ?>"</p>
+        <form action="..." method="post">
+            <input name="productID" value="<?php echo $row["id"]; ?>" type="hidden">
+            <input name="qty" type="number" placeholder="QTY">
+            <input type="submit" value="Add to cart">
+        </form>
     </div>
     <?php
 }
--- include/register.php
+++ include/register.php
@@ -5,24 +5,28 @@ $username = $_POST["username"];
 $password = $_POST["password"];
 $address = $_POST["address"];
 $phone = $_POST["phone"];
+if ($firstname == "" || $lastname==""){
+    //1. connect to database
+    $server = "klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $dbusername = "t8jnow42fmp1smpt";
+    $dbpassword = "fdavedw769oxw5pd";
+    $dbname = "k2nfay1osz1i59kc";
 
-//1. connect to database
-$server = "klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$dbusername = "t8jnow42fmp1smpt";
-$dbpassword = "fdavedw769oxw5pd";
-$dbname = "k2nfay1osz1i59kc";
-
-$conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
+    $conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
 
 //2. create a query
-$sql = "INSERT INTO `users`(`id`, `firstname`, `lastname`, `username`, `password`, `address`, `phoneNumber`) 
+    $sql = "INSERT INTO `users`(`id`, `firstname`, `lastname`, `username`, `password`, `address`, `phoneNumber`) 
 VALUES (NULL,'$firstname','$lastname','$username','$password','$address','$phone')";
 
 //3. run the query
-if (mysqli_query($conn, $sql)){
-    echo "data has been inserted";
-}else{
-    echo "data has not been inserted";
+    if (mysqli_query($conn, $sql)){
+        echo "data has been inserted";
+    }else{
+        echo "data has not been inserted";
+    }
+}
+else{
+    echo "there should not be empty input";
 }
 
 
