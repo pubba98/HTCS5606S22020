b6dd00d1161af327abe1923b850fdb6450ef0c7a
Lei <gabriel_sl19798@hotmail.com>
2020-11-18 10:02:50 +1300

    create 3 functions, one connect to database, one get product name by id, another one get product price by id

--- include/cart.php
+++ include/cart.php
@@ -8,6 +8,61 @@ $i = 0;
 while ($i<sizeof($orderedProductIDs)){
     $orderedProductID = $orderedProductIDs[$i];
     $orderedProductQty = $orderedProductQtys[$i];
-    echo "<p>ID: $orderedProductID and Qty: $orderedProductQty</p>";
+    $productName = getProductNameByProductID($orderedProductID);
+    $price=getProductPriceByProductID($orderedProductID);
+    echo "<p>Name: $productName Qty: $orderedProductQty Price: $price</p>";
     $i++;
+}
+
+
+function createDatabaseConnection(){
+    //1. connect to database
+    $server = "klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $dbusername = "t8jnow42fmp1smpt";
+    $dbpassword = "fdavedw769oxw5pd";
+    $dbname = "k2nfay1osz1i59kc";
+
+    $conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
+    return $conn;
+}
+
+/**
+ * @name getProductNameByProductID
+ * @param $productID
+ * @return product name
+ */
+function getProductNameByProductID($productID){
+    //1. create a db connection
+    $conn = createDatabaseConnection();
+    //2. query
+    $sql = "select name from product where id=$productID";
+    //3. run query
+    $result = mysqli_query($conn, $sql);
+
+    //4. show result
+    while ($row=$result->fetch_assoc()){
+        $name = $row["name"];
+    }
+    return $name;
+}
+
+
+/**
+ * @name getProductPriceByProductID
+ * @param $productID
+ * @return product price
+ */
+function getProductPriceByProductID($productID){
+    //1. create a db connection
+    $conn = createDatabaseConnection();
+    //2. query
+    $sql = "select price from product where id=$productID";
+    //3. run query
+    $result = mysqli_query($conn, $sql);
+
+    //4. show result
+    while ($row=$result->fetch_assoc()){
+        $price = $row["price"];
+    }
+    return $price;
 }
